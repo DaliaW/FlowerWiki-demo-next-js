90c27759f8590778e033b3f8c9857175a8cab3f2
Dalia Walid <47950134+DaliaW@users.noreply.github.com>
2021-05-01 06:34:27 +0200

    Update [id].js

--- myapp/pages/flowers/[id].js
+++ myapp/pages/flowers/[id].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router'
 
-import Head from 'next/Head'
+import Head from 'next/head'
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
@@ -9,7 +9,6 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import styles from '../../styles/Home.module.css'
 
